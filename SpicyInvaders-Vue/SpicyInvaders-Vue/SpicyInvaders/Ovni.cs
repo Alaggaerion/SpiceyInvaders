0300300be65e64daa0db4072befea04c4b8f22a3
Alaggaerion <sly.laydernier@yahoo.fr>
2017-01-30 15:16:29 +0100

    vue
    added message "hello worlds!" in accueil class

---
+++ SpicyInvaders-Vue/SpicyInvaders-Vue/SpicyInvaders/Ovni.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpicyInvaders
+{
+    public class Ovni : Vue
+    {
+    }
+
+    public class Copy1OfOvni
+    {
+        private int LIMITE_GAUCHE;
+        private int LIMITE_DROITE;
+
+        public int deplaceAxeX
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public char modele
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int vitesse
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void deplaceEnnemi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void explosionEnnemi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void calculPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void apparitionOvni()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int afficherPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class CopyOfOvni
+    {
+        private int LIMITE_GAUCHE;
+        private int LIMITE_DROITE;
+
+        public int deplaceAxeX
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public char modele
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int vitesse
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void deplaceEnnemi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void explosionEnnemi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void calculPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void apparitionOvni()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int afficherPoints()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
